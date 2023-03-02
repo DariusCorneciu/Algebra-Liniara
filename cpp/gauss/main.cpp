19250a0585f17dd2841dff0476dae2b5532d37c4
Darius Corneciu <116907008+DariusCorneciu@users.noreply.github.com>
2023-03-02 02:01:35 +0200

    Add files via upload

---
+++ cpp/gauss/main.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+ifstream f("C:\\Users\\Darius Corneciu\\CLionProjects\\algebraliniara\\cpp\\traditional\\matrix.txt");
+
+int main()
+{
+    double matrix[100][100], det = 1,n;
+
+    f>>n;
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            f >> matrix[i][j];
+        }
+    }
+
+    for (int i = 0; i < n; ++i)
+    {
+        bool ok = false;
+        if (matrix[i][i] == 0)
+        {
+            ok = true;
+            for (int j = i; j < n; ++j)
+            {
+                if (matrix[j][i] != 0) {
+                    det *= -1;
+                    for (int k = 0; k < n; ++k) {
+                        double t = matrix[i][k];
+                        matrix[i][k] = matrix[j][k];
+                        matrix[j][k] = t;
+                        ok = false;
+                    }
+                }
+            }
+        }
+
+        if (ok) {
+            det = 0;
+            break;
+        }
+
+        else {
+
+            for (int j = i+1; j < n; ++j)
+            {
+                double retin = matrix[j][i];
+                for (int k = i; k < n; ++k) {
+                    matrix[j][k] -= (matrix[i][k]*retin)/matrix[i][i];
+
+                }
+            }
+            det *= matrix[i][i];
+        }
+    }
+
+    cout << "Determinant: " << det;
+    return 0;
+}
